00b26bd145721b7dc9e0f2c2c73ce388a9bce1aa
Luca Corti <luca@fantacast.it>
2020-09-07 02:01:32 +0200

    Support elixir 1.8+

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Dredd.MixProject do
     [
       app: :dredd,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
